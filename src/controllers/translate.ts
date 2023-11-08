4b35507409c13ec54a95e6f14a3821db26a31831
Cactus322 <momchyan1997@mail.ru>
2023-11-08 15:05:32 +0300

    create proxy

--- src/controllers/translate.ts
+++ src/controllers/translate.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 import { ITranslateRequest } from '../types'
 import { translate } from '@vitalets/google-translate-api'
+import { HttpProxyAgent } from 'http-proxy-agent'
 
 const translateRouter = express.Router()
 
@@ -8,10 +9,21 @@ translateRouter.post(
 	'/',
 	async (request: Request<{}, {}, ITranslateRequest>, response: Response) => {
 		const { word } = request.body
+		const timeoutMs = 5000;
+		const ac = new AbortController()
+		const timer = setTimeout(() => ac.abort(), timeoutMs)
 
-		const { text } = await translate(word, { to: 'en' })
-
-		return response.status(201).json(text)
+		const fetchOptions = {
+			agent: new HttpProxyAgent('http://213.233.177.134:80'),
+			signal: ac.signal,
+		} 
+		
+		try {
+			const { text } = await translate(word, {to: 'ru', fetchOptions })
+			return response.status(201).json(text)
+		} finally {
+			clearTimeout(timer)
+		}
 	}
 )
 
